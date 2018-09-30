d2b6090d61ed8a14415e6acf7400e84910906fb6
Srinivasulu M <srinipro@yahoo.com>
2018-09-30 06:07:47 +0530

    Update mysql.connection.ts

--- src/database/mysql.connection.ts
+++ src/database/mysql.connection.ts
@@ -8,11 +8,7 @@ export class MySql {
             connectionLimit: 10,
             host: 'localhost',
             user: 'root',
-<<<<<<< HEAD
             password: '****',
-=======
-            password: '******',
->>>>>>> 654ef71dad46eb5793febb4945210a3ef9bfabae
             database: 'demo'
         });
        // console.log('MySqlConnection Constructor called');
